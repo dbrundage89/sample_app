3a5d8d868f5ddb32a640069a2d361e2eddab3e40
David Brundage <dbrundage89@users.noreply.github.com>
2019-11-11 21:26:23 -0500

    dry up code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'about', to: 'static_pages#about'
+  get 'static_pages/contact'
   # get '/patients/:id', to: 'patients#show', as: 'patient'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
